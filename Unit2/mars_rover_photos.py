b81af4f97a8c5072ad6ef66d51a78cde509a38c2
jusbro <97056196+jusbro@users.noreply.github.com>
2024-03-24 17:26:45 -0400

    Create mars_rover_photos.py

---
+++ Unit2/mars_rover_photos.py
@@ -0,0 +1,10 @@
+#Mars Photo Viewer
+#The following code will return photos from Mars taken from NASAs Curiosity, Opportunity, and Spirit rovers
+
+#Visit this page for the API calls from NASA. Browse APIs, then select Mars Rover Photos
+#https://api.nasa.gov/
+
+#in the space below, create your code that reaches the desired outcome. You may use old code of yours to accomplish this task as you see fit
+
+#YOU MAY NOT USE MORE THAN 35 LINES OF CODE. COMMENTS AND BLANK LINES DO NOT COUNT TOWARDS THIS REQUIRMENT
+#YOU MUST HAVE AT LEAST 1 COMMENT LINE FOR EVERY 4 LINES OF CODE
